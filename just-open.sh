51198d5ef57e3a33a9f4533c4faa1a265d33dade
Dmitry V. Luciv <dluciv@dluciv.name>
2016-06-10 22:21:55 +0300

    Make it more secure. Could run any command before.

--- just-open.sh
+++ just-open.sh
@@ -5,11 +5,5 @@ target=${target/just-open\:/}
 
 xdg-open "$target"
 if [ $? == 127 ]; then
-  echo No xdg-open, likely running under Windows. Achtung!
-  cmd /c start "" "$target"
-  cmd_result=$?
-  if [ $cmd_result != 0 ]; then
-    echo Windows open URL result: $cmd_result
-    read
-  fi
+  echo No xdg-open. Achtung!
 fi
