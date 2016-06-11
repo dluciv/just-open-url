8a7b0b9f5ea8618b12703dd437ae9d9fd0572e55
Dmitry V. Luciv <dluciv@dluciv.name>
2016-06-11 13:22:59 +0300

    Corrected protocols

--- just-open.js
+++ just-open.js
@@ -2,7 +2,7 @@
   var wsh = WScript.CreateObject("WScript.Shell");
   var parm = WScript.Arguments.Item(0);
   // be secure about allowed protocols
-  var rmatch = parm.match(/^just-open:((http|https|ftp|gopher|xmpp|magnet):\/\/.+)$/);
+  var rmatch = parm.match(/^just-open:((http:\/\/|https:\/\/|ftp:\/\/|gopher:\/\/|xmpp:|magnet:|mailto:).+)$/);
   if(rmatch) {
     var target = rmatch[1];
     // WScript.Echo(target);
