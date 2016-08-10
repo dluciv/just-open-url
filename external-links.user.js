9f04419542e223d86142b3a3928462c4a88c64ee
Dmitry V. Luciv <dluciv@dluciv.name>
2016-08-10 13:25:40 +0300

    More and less agressive link modification

--- external-links.user.js
+++ external-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Messenger like Matrix and Skype external URLs
 // @namespace    http://dluciv.name/
-// @version      0.0.1
+// @version      0.0.2
 // @description  Intercept all external links to be handled by just-open scheme
 // @author       Dmitry V. Luciv
 // @match        http*://vector.im/beta/*
@@ -11,13 +11,26 @@
 
 (function() {
     'use strict';
-    document.body.onclick = function(evt) {
-        if(evt.path && evt.path[0].localName == 'a' && evt.path[0].target == '_blank') {
-            var hr = evt.path[0].href;
-            if(hr) {
+    function url_domain(data) {
+        var a = document.createElement('a');
+        a.href = data;
+        return a.hostname;
+    }
+
+    var updateLink = function(a) {
+        if(a.target == '_blank' && a.href.indexOf("just-open:") !== 0 && url_domain(a.href) != url_domain(window.location.href)) {
+            // unvisited external link
+            a.setAttribute("onclick", function(evt){
                 evt.preventDefault();
-                window.location.assign("just-open:" + hr);
-            }
+                evt.stopPropagation();
+            });
+            a.setAttribute("target", "_self");
+            a.setAttribute("href", "just-open:" + a.href);
         }
+    };
+
+    window.onmouseover = function(evt) {
+        if(evt.target.localName == "a")
+            updateLink(evt.target);
     };
 })();
