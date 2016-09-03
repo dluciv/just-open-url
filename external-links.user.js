55945a0bdbbcb3b7eb7a34c6bca9ae85656ab6af
Dmitry V. Luciv <dluciv@dluciv.name>
2016-09-03 13:33:06 +0300

    Revert "More and less agressive link modification"
    This reverts commit 9f04419542e223d86142b3a3928462c4a88c64ee.

--- external-links.user.js
+++ external-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Messenger like Matrix and Skype external URLs
 // @namespace    http://dluciv.name/
-// @version      0.0.2
+// @version      0.0.1
 // @description  Intercept all external links to be handled by just-open scheme
 // @author       Dmitry V. Luciv
 // @match        http*://vector.im/beta/*
@@ -12,26 +12,13 @@
 
 (function() {
     'use strict';
-    function url_domain(data) {
-        var a = document.createElement('a');
-        a.href = data;
-        return a.hostname;
-    }
-
-    var updateLink = function(a) {
-        if(a.target == '_blank' && a.href.indexOf("just-open:") !== 0 && url_domain(a.href) != url_domain(window.location.href)) {
-            // unvisited external link
-            a.setAttribute("onclick", function(evt){
+    document.body.onclick = function(evt) {
+        if(evt.path && evt.path[0].localName == 'a' && evt.path[0].target == '_blank') {
+            var hr = evt.path[0].href;
+            if(hr) {
                 evt.preventDefault();
-                evt.stopPropagation();
-            });
-            a.setAttribute("target", "_self");
-            a.setAttribute("href", "just-open:" + a.href);
+                window.location.assign("just-open:" + hr);
+            }
         }
-    };
-
-    window.onmouseover = function(evt) {
-        if(evt.target.localName == "a")
-            updateLink(evt.target);
     };
 })();
