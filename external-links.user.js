8e6bb5587fe8b220d05f24ed5a4b17b838b9dbe7
Dmitry V. Luciv <dluciv@dluciv.name>
2016-09-03 13:36:15 +0300

    returned to location assign model

--- external-links.user.js
+++ external-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Messenger like Matrix and Skype external URLs
 // @namespace    http://dluciv.name/
-// @version      0.0.1
+// @version      0.0.3
 // @description  Intercept all external links to be handled by just-open scheme
 // @author       Dmitry V. Luciv
 // @match        http*://vector.im/beta/*
@@ -13,11 +13,11 @@
 (function() {
     'use strict';
     document.body.onclick = function(evt) {
-        if(evt.path && evt.path[0].localName == 'a' && evt.path[0].target == '_blank') {
-            var hr = evt.path[0].href;
+        if(evt.path && evt.target.localName == 'a' && evt.target.target == '_blank') {
+            var hr = evt.target.href;
             if(hr) {
-                evt.preventDefault();
                 window.location.assign("just-open:" + hr);
+                return false;
             }
         }
     };
